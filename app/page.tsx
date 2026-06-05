c77feb0f58712eaca56f05cb5f35e1a716494b55
diaby007 <kassambasouleymane8@gmail.com>
2026-06-05 07:18:27 +0200

    feat: Include AlbumInfoSection in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
+import AlbumInfoSection from "@/components/AlbumInfoSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <AlbumInfoSection />
     </main>
   );
 }
